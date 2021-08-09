93b55e554e8e072d47380c276486a15069d40a75
Eldar Gabdullin <eldargab@gmail.com>
2021-08-10 00:43:11 +0300

    complete example

--- mappings/index.ts
+++ mappings/index.ts
@@ -1,4 +1,4 @@
-import { SumReward } from '../generated/model'
+import { SumReward, NoBondRecordAccount, StakingReward, StakingSlash } from '../generated/model'
 import { Staking } from '../types/index' // import via index.ts, this is a workaround related to ts-node
 import BN from 'bn.js'
 import {
@@ -10,17 +10,77 @@ import {
 
 export async function handleBond({
   store,
-  event,
-  block,
-  extrinsic,
+  event
 }: EventContext & StoreContext) {
 
   const [account, balance] = new Staking.BondedEvent(event).params
 
-  const e = await store.get(SumReward, {where: {id: account.toString()}})
+  const e = await get(store, SumReward, account.toString())
   if (e == null) {
     store.save(createSumReward(account.toString()))
   }
+}
+
+
+export async function handleReward({
+  store,
+  event,
+  block
+}: EventContext & StoreContext) {
+  const [account, newReward] = new Staking.RewardEvent(event).params
+
+  let sumReward = await get(store, SumReward, account.toString());
+  if (sumReward == null){
+      // in early stage of kusama, some validators didn't need to bond to start staking
+      // to not break our code, we will create a SumReward record for them and log them in NoBondRecordAccount
+      sumReward = createSumReward(account.toString());
+      const errorRecord = new NoBondRecordAccount();
+      errorRecord.id = account.toString()
+      errorRecord.firstRewardAt = block.height
+      store.save(errorRecord)
+  }
+
+  sumReward.accountReward = sumReward.accountReward.add(new BN(newReward.toString()))
+  sumReward.accountTotal = sumReward.accountReward.sub(sumReward.accountSlash)
+  store.save(sumReward)
+
+  const reward = new StakingReward()
+  reward.id = `${block.height}-${event.indexInBlock}`
+  reward.address = account.toString()
+  reward.balance = new BN(newReward.toString())
+  reward.date = new Date(event.blockTimestamp)
+  store.save(reward)
+}
+
+
+export async function handleSlash({
+  store,
+  event,
+  block,
+}: EventContext & StoreContext): Promise<void> {
+
+  const [account, newSlash] = new Staking.SlashEvent(event).params
+
+  let sumReward = await get(store, SumReward, account.toString())
+  if (sumReward == null){
+      // in early stage of kusama, some validators didn't need to bond to start staking
+      // to not break our code, we will create a SumReward record for them and log them in NoBondRecordAccount
+      sumReward = createSumReward(account.toString());
+      const errorRecord = new NoBondRecordAccount()
+      errorRecord.id = account.toString()
+      errorRecord.firstRewardAt = block.height
+      store.save(errorRecord)
+  }
+  sumReward.accountSlash = sumReward.accountSlash.add(new BN(newSlash.toString()))
+  sumReward.accountTotal = sumReward.accountReward.sub(sumReward.accountSlash)
+  store.save(sumReward)
+
+  const slash = new StakingSlash()
+  slash.id = `${block.height}-${event.indexInBlock}`
+  slash.address = account.toString();
+  slash.balance = new BN(newSlash.toString())
+  slash.date = new Date(block.timestamp)
+  store.save(slash)
 }
 
 
@@ -31,4 +91,12 @@ function createSumReward(accountId: string): SumReward {
   entity.accountSlash = new BN(0)
   entity.accountTotal = new BN(0)
   return entity;
+}
+
+type EntityType<T> = {
+  new (...args: any[]): T
+}
+
+function get<T>(store: StoreContext['store'], entityType: EntityType<T>, id: string): Promise<T | undefined> {
+  return store.get(entityType, {where: {id}})
 }
